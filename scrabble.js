50ab6f13005082fbeafaf8dbd5e4ff33c20afac1
Alma Maizels <aymaiz@yahoo.com>
2016-11-16 11:55:21 -0800

    Reqs completed

--- scrabble.js
+++ scrabble.js
@@ -6,6 +6,8 @@ var Scrabble = function() {
 
 };
 
+// SCORING
+
 var Scoring = function() {
   this.tiles = {
     "A": 1,"E": 1,"I": 1,"O": 1,"U": 1,"L": 1,"N": 1,"R": 1,"S": 1,"T": 1,
@@ -15,7 +17,7 @@ var Scoring = function() {
 
 Scoring.prototype.score = function(word) {
   // invalidWords.push(word); // global variable?
-  this.word = word;
+  this.word = (word.toUpperCase());
   var score = 0;
   for (var i=0; i<this.word.length;i++) {
     score += this.tiles[this.word[i]];
@@ -54,27 +56,89 @@ Scoring.prototype.highestScoreFrom = function(arrayOfWords) {
   return winningWord;
 };
 
-var my7Word = new Scoring(); // No checks for upper case letters
+var my7Word = new Scoring();
 var hereIsMyScore = my7Word.score('PIZZAZZ');
 
 console.log(hereIsMyScore);
 
-//COMMENTING FOR TROUBLESHOOTING
-var wordScoring = new Scoring(); // it is a bit weird to feed this with an empty string here
+
+var wordScoring = new Scoring();
 var winWord = wordScoring.highestScoreFrom(['PIZZAZZ', 'EA', 'EZY']);
 console.log(winWord);
 
-// Scrabble.prototype.helloWorld = function() {
-//   return 'hello world!';
-//   // return 5;
-// };
+// PLAYER
+// =================
 
-// var myScrabble = new Scrabble;
-// var callOut = myScrabble.helloWorld() + 'hey you';
-// // var callOut = myScrabble.helloWorld() + 50;
-//
-// console.log(callOut);
+var Player = function(name) {
+  this.name = name;
+  this.wordsPlayed = [];
+  this.score = 0;
+  this.winner = false;
+};
+
+Player.prototype.plays = function () {
+  return this.wordsPlayed;
+};
+
+Player.prototype.play = function (word) {
+  if (this.winner === true) {
+    return false;
+  }
+  this.word = word;
+  this.score = new Scoring().score(this.word);
+  this.wordsPlayed.push(this.word);
+  return this.score;
+};
+
+Player.prototype.totalScore = function () {
+  var playerScore = 0;
+  for (var i = 0; i < this.wordsPlayed.length; i++) {
+    var word = this.wordsPlayed[i];
+    var currentScore = new Scoring().score(word);
+    playerScore += currentScore;
+  }
+  return playerScore;
+};
+
+Player.prototype.hasWon = function () {
+  if (this.totalScore() >= 100) {
+      return this.winner = true;
+  } else {
+      return this.winner = false;
+  }
+};
+
+Player.prototype.highestScoringWord = function () {
+  var bestWord = new Scoring().highestScoreFrom(this.wordsPlayed);
+  return bestWord;
+};
+
+//highestWordScore(): Function which returns the highestScoringWord score
+Player.prototype.highestWordScore = function () {
+  var highestWord = this.highestScoringWord();
+  return new Scoring().score(highestWord);
+};
 
 
+var player = new Player('Xerxes');
+console.log(player.name);
+
+var wordScore = player.play('PIZZAZZ');
+console.log(wordScore);
+console.log(player.name + ' is a winner? ' + player.hasWon());
+
+var playerWords = ['PIZZAZZ', 'PIZZAZZ', 'PIZZAZZ'];
+player.wordsPlayed = playerWords;
+var totScore = player.totalScore();
+console.log(totScore + ' points for player: ' + player.name);
+console.log(player.name + ' is a winner? ' + player.hasWon());
+
+var cat = new Player('Stellar');
+var catWords = ['meoow','mjaoo','foood','oouut'];
+cat.wordsPlayed = catWords;
+console.log(cat.name + ' played ' + cat.highestScoringWord() + ' for ' + cat.highestWordScore() + ' points.');
+
+
+// =================
 
 module.exports = Scrabble;
